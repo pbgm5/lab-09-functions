7ea7ab9f6a84baa80c8db9f0e5297c375505be47
pbgm5 <Priya.Begum@mail.citytech.edu>
2021-04-12 16:26:18 -0400

    addeddddd

---
+++ introduce.py
@@ -0,0 +1,7 @@
+def introduce(person1, person2):
+    print("Hello " + person1 + " this is " + person2 + ".")
+
+p1 = input("name a person: ")
+p2 = input("name another person: ")
+
+introduce(p1, p2)
